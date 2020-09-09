d01392129fe54b009fdc3d4201268866c83963b2
cool5tar <cool5tarXV@gmail.com>
2020-09-09 12:07:26 +0000

    added curl bash script

---
+++ curl/tests/dl-all.bash
@@ -0,0 +1,2 @@
+#Completed for curl script
+#run curl -fsSL https://raw.githubusercontent.com/cool5tar/Make-Your-Sileo/master/dl-all.bash
--- dl-all.bash
+++ dl-all.bash
@@ -1,59 +1,30 @@
-echo "this is for people who dont have git and are using wget..."
+#!/usr/bin/bash
 
-echo 'starting...'
+appName="makeyoursileo"
+appSite="https://raw.githubusercontent.com/cool5tar/Make-Your-Sileo/master/dl-all.bash"
+appVer="V1.0 Beta 1"
+appDevs=( Cool5tar Sn0wstar )
 
-sleep 1
+checkDeb() {
+	if ! command -v apt &> /dev/null
+	then
+		echo ""
+		echo "ERROR: Non-debian system detected. Exiting..."
+		exit 1
+	fi
+}
+checkRoot() {
+	if [ "$EUID" -ne 0 ]
+	then
+		echo ""
+		echo "ERROR: Not running as root. Please prefix this script with sudo. Exiting..."
+		exit 2
+	fi
+}
 
+echo "made by.."
+echo "cool5tar aka Sn0wstar"
+echo "starting..."
 cd ~
-
-mkdir Make-Your-Sileo
-
-cd Make-Your-Sileo
-
-echo 'downloading assets...'
-
-sleep 2
-
-echo 'downloading README.md...'
-
-wget https://raw.githubusercontent.com/cool5tar/Make-Your-Sileo/master/README.md
-
-echo 'downloading menu...'
-
-wget https://raw.githubusercontent.com/cool5tar/Make-Your-Sileo/master/main.sh
-
-echo 'downloading git installer...'
-
-wget https://raw.githubusercontent.com/cool5tar/Make-Your-Sileo/master/git.sh
-
-echo 'downloading gem installer...'
-
-wget https://raw.githubusercontent.com/cool5tar/Make-Your-Sileo/master/gem.sh
-
-echo 'downloading installer for git,gem and source...'
-
-wget https://raw.githubusercontent.com/cool5tar/Make-Your-Sileo/master/install-all.sh
-
-echo 'downloading source menu...'
-
-wget https://raw.githubusercontent.com/cool5tar/Make-Your-Sileo/master/source-menu.sh
-
-echo 'downloading source installer...'
-
-wget https://raw.githubusercontent.com/cool5tar/Make-Your-Sileo/master/sourceinstall.sh
-
-echo 'downloading removal script...'
-
-wget https://raw.githubusercontent.com/cool5tar/Make-Your-Sileo/master/rm.sh
-
-echo 'downloading updater...'
-
 wget https://raw.githubusercontent.com/cool5tar/Make-Your-Sileo/master/dl-all.sh
-
-echo 'done...'
-
-echo 'starting installer...'
-
-sleep 2
-
-bash main.sh
+bash dl-all.sh
