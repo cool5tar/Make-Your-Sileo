78d07f477c7666ca1673adcf7d9f1f1723bc7d62
cool5tar <cool5tarXV@gmail.com>
2020-09-10 03:02:36 +0000

    added uninstall prompt

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,17 @@
-cd ~
+read -r -p "Do you want to start uninstall? [Y/n] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        cd ~
+        rm -rf Make-Your-Sileo
+        ;;
+    *)
+        echo "stopping script..."
+        break
+        ;;
+esac
 
-rm -rf Make-Your-Sileo
+
+
+
+
+
